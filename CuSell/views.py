e3b70090cad7b8eefa2e1191d2b4c9371c0be1cb
1155141394 <1155141394@link.cuhk.edu.hk>
2022-03-05 20:44:47 +0800

    add post method

--- CuSell/views.py
+++ CuSell/views.py
@@ -1,14 +1,23 @@
 from django.shortcuts import render
-
+from django.shortcuts import HttpResponse
 # Create your views here.
 def index(request):
     
     return render(request, 'mainpage.html')
 
 def reg(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        print(email,username,password) 
     return render(request,'registration.html')
 
 def login(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        print(email,password)
     return render(request,'login.html')
 
 def error(request):
