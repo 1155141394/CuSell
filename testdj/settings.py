fdcac885d17000d082536ec58cbc601bc19a405d
1155141394 <1155141394@link.cuhk.edu.hk>
2022-04-03 19:05:55 +0800

    yes

--- testdj/settings.py
+++ testdj/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'CuSell',
         'USER': 'root',
-        'PASSWORD': 'Csci3100^',
+        'PASSWORD': '#tao134010',
         'HOST': '127.0.0.1',
         'PORT': '3306'
     }
